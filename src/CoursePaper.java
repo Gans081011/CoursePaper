e8cb19ed2ae1a03972cc9ed79262dca06e030b85
Вячеслав Блинков <xristian860810@gmail.com>
2022-07-20 16:39:27 +0300

    Курсовая выполнена, правки внесены повторно. Добавлен метод printSumSalary, что исправляет работу averageSalary. Исправлены условия методов, в случае, если ячейчка массива пуста

--- src/CoursePaper.java
+++ src/CoursePaper.java
@@ -16,20 +16,29 @@ public class CoursePaper {
         for (Employee employee : employees) {
             if (employee != null) {
                 sumSalary = sumSalary + employee.getSalary();
-            } else {
-                System.out.println("массив под этим индексом пуст");
-                break;
             }
         }
-        System.out.println("Сумма затрат на зарплаты в месяц: " + sumSalary);
+        return sumSalary;
+    }
+
+    public static float printSumSalary(Employee[] employees) {
+        float sumSalary = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                sumSalary = sumSalary + employee.getSalary();
+            }
+        }
+        System.out.println("Сумма затрат на зарплаты в месяц:" + sumSalary);
         return sumSalary;
     }
 
     public static float findMinSalary(Employee[] employees) {
         float minSalary = employees[0].getSalary();
-        for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            if (minSalary > employees[i].getSalary() && employees[i] != null) {
-                minSalary = employees[i].getSalary();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (minSalary > employees[i].getSalary() && employees[i] != null) {
+                    minSalary = employees[i].getSalary();
+                }
             }
         }
         System.out.println("Минимальная зарплата равна:" + minSalary);
@@ -39,9 +48,11 @@ public class CoursePaper {
 
     public static float findMaxSalary(Employee[] employees) {
         float maxSalary = employees[0].getSalary();
-        for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            if (maxSalary < employees[i].getSalary()) {
-                maxSalary = employees[i].getSalary();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (maxSalary < employees[i].getSalary()) {
+                    maxSalary = employees[i].getSalary();
+                }
             }
         }
         System.out.println("Максимальная зарплата равна:" + maxSalary);
@@ -56,25 +67,37 @@ public class CoursePaper {
     }
 
     public static void listOfFullNamesOfEmployees(Employee[] employees) {
-        for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            System.out.println("ФИО сотрудника " + employees[i].getFirstName() + " " + employees[i].getLastName() + " " + employees[i].getPatronymic());
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                System.out.println("Массив под этим индексом пуст");
+            } else {
+                System.out.println("ФИО сотрудника " + employees[i].getFirstName() + " " + employees[i].getLastName() + " " + employees[i].getPatronymic());
+            }
         }
     }
 
     public static void printNewSalaryAll(Employee[] employees) {
         float arguments = 0.03f; // величина аргумента в процентах
-        for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            employees[i].setSalary(employees[i].getSalary() * arguments + employees[i].getSalary());
-            System.out.println("Новая зарплата сотрудника после индексации: " + employees[i].getSalary());
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                System.out.println("Массив под этим индексом пуст");
+            } else {
+                employees[i].setSalary(employees[i].getSalary() * arguments + employees[i].getSalary());
+                System.out.println("Новая зарплата сотрудника id№ " + employees[i].getId() + " после индексации: " + employees[i].getSalary());
+            }
         }
     }
 
     public static int printNewSalaryForNumDepartment(int numDepartment) {
         float arguments = 0.1f; // величина аргумента в процентах
-        for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            if (employees[i].getNumDepartment() == numDepartment) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].getNumDepartment() == numDepartment) {
                 employees[i].setSalary(employees[i].getSalary() * arguments + employees[i].getSalary());
-                System.out.println("Новая зарплата сотрудника " + numDepartment + " отдела после индексации: " + employees[i].getSalary());
+                System.out.println("Новая зарплата сотрудника id№ " + employees[i].getId() + " " + numDepartment + " отдела после индексации: " + employees[i].getSalary());
+            } else if (employees[i] != null) {
+                System.out.println("Сотрудники из других отделов");
+            } else if (employees[i] == null) {
+                System.out.println("Ячейка в массиве сотрудника пуста");
             }
         }
         return numDepartment;
@@ -83,19 +106,21 @@ public class CoursePaper {
     public static void toFindMinSalaryForNumDepartment(int numDepartment) {
         float minSalary = 0;
 
-        for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            if (employees[i].getNumDepartment() == numDepartment) {
-                minSalary = employees[i].getSalary();
-            }
-            if (minSalary > employees[i].getSalary() && employees[i].getNumDepartment() == numDepartment) {
-                minSalary = employees[i].getSalary();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (employees[i].getNumDepartment() == numDepartment) {
+                    minSalary = employees[i].getSalary();
+                }
+                if (minSalary > employees[i].getSalary() && employees[i].getNumDepartment() == numDepartment) {
+                    minSalary = employees[i].getSalary();
+                }
             }
         }
         System.out.println("Минимальная зарплата по отделу " + numDepartment + " равна:" + minSalary);
     }
 
     public static void main(String[] args) {
-        employees[0] = new Employee("Брусков", "Иван", "Григорьевич", 3200, 2);
+        employees[0] = new Employee("Погрелов", "Вениамин", "Афанасьевич", 12000, 2);
         employees[1] = new Employee("Горелов", "Ян", "Анатольевич", 2400, 1);
         employees[2] = new Employee("Лепешкин", "Алексей", "Владимироч", 1500, 2);
         employees[3] = new Employee("Крылов", "Владимир", "Александрович", 10000, 1);
